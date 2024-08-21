b54db83c771e7f95f872cbfe91bfacd5cfda6030
PHILIPORACOMA <epicarusracoma@gmail.com>
2024-08-21 18:40:15 +0800

    Add files via upload

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,6 @@
+import javax.swing.JOptionPane;
+class SwingAlert {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello Universe! yeah buddyy! lightweight!", "Message", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
